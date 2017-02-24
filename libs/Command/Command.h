f91b5ecab5d003ccf8c92e89ce331d7973775aa1
Ondrej Telka <ondrejtelka@gmail.com>
2017-02-24 01:26:07 +0100

    Added some more encoding and decoding methods
    void setIdentifier(String identifier);
    void setParam(const char parameterIdentifier, int value);
    void setParam(const char parameterIdentifier, float value);
    void setParam(const char parameterIdentifier, String value);
    void delParam(const char parameterIdentifier);
    bool hasParam(const char parameterIdentifier);

--- libs/Command/Command.h
+++ libs/Command/Command.h
@@ -6,19 +6,29 @@ class Command {
     Command(char &m);
     Command();
     
+    void set(String s);
     void readSerial();
     String getIdentifier();
-    String getParameter(int nth);
-    String getParameterValue(const char parameterIdentifier);
+    void setIdentifier(String identifier);
+    String getParam(const char parameterIdentifier);
+    void setParam(const char parameterIdentifier, int value);
+    void setParam(const char parameterIdentifier, float value);
+    void setParam(const char parameterIdentifier, String value);
+    void delParam(const char parameterIdentifier);
+    bool hasParam(const char parameterIdentifier);
+    
     
     String toString();
     
     //bool verify();
     
     
-    String msg = "";
+    
     
   private:
+  
+    String msg = "";
+  
     bool isUppercaseLetter(char m);
     
     bool ok = false;
@@ -102,39 +112,24 @@ void Command::readSerial() {
     }
 }
 
-
-
-
-
-
-
-
 String Command::getIdentifier() {
     return (String)msg.charAt(1) + (String)msg.charAt(2) + (String)msg.charAt(3);
 }
 
-
-
-String Command::getParameterValue(const char parameterIdentifier) {
-    String str;
-    
-    if (isUppercaseLetter(parameterIdentifier)) {
-        for (int i = 0; str != "#"; i++) {
-            str = getParameter(i);
-            if (str.charAt(0) == parameterIdentifier) {
-                str.remove(0, 1);
-                return str;
-            }
+void Command::setIdentifier(String identifier) {
+    if (isUppercaseLetter(identifier.charAt(0)) && isUppercaseLetter(identifier.charAt(1)) && isUppercaseLetter(identifier.charAt(2))) {
+        if (msg == "") {
+            msg = startChar + identifier + endChar;
+        }
+        else {
+            msg.setCharAt(1, identifier.charAt(0));
+            msg.setCharAt(2, identifier.charAt(1));
+            msg.setCharAt(3, identifier.charAt(2));
         }
     }
-    
-    return "";
 }
 
-
-
-// Get n-th parameter of command
-String Command::getParameter(int nth) {
+String Command::getParam(const char parameterIdentifier) {
     int length = msg.length() + 1;
     int start, round = 0;
     //bool cmdAsParameter = false;
@@ -144,18 +139,19 @@ String Command::getParameter(int nth) {
         if ((char)msg.charAt(i) == startChar) {
             while ((char)msg.charAt(i) != endChar) {
                 i++;
+				// TODO: cmdAsParameter = true;
             }
         }
         
-        if (isUppercaseLetter(msg.charAt(i))) {
-            if (round == nth) {
-                start = i;
-                break;
-            }
-            round++;
+        if (isUppercaseLetter(msg.charAt(i)) && msg.charAt(i) == parameterIdentifier) {
+            start = i + 1;
+            break;
         }
     }
     
+    //start = msg.substring(4).indexOf(parameterIdentifier) + 1;
+    
+      
     String param = (String)msg.charAt(start);
     for (int i = start + 1; !isUppercaseLetter(msg.charAt(i)); i++) {
         if ((char)msg.charAt(i) == endChar) {
@@ -174,15 +170,90 @@ String Command::getParameter(int nth) {
     return param;
 }
 
+void Command::set(String s) {
+    msg = s;
+}
+
+void Command::setParam(const char parameterIdentifier, int value) {
+    setParam(parameterIdentifier, (String)value);
+}
+
+void Command::setParam(const char parameterIdentifier, float value) {
+    setParam(parameterIdentifier, (String)value);
+}
+
+void Command::setParam(const char parameterIdentifier, String value) {
+    if (msg == "" || msg.length() < 5) return;
+
+    for (int i = 4; i <= msg.length(); i++) {
+        if ((char)msg.charAt(i) == startChar) {
+            while ((char)msg.charAt(i) != endChar) {
+                i++;
+                // TODO: cmdAsParameter = true;
+            }
+        }
+
+        if (isUppercaseLetter(msg.charAt(i)) && msg.charAt(i) == parameterIdentifier) {
+            String a = msg.substring(0, i + 1);
+            String b = msg.substring(i + 1);
+
+            while (!isUppercaseLetter(b.charAt(0))) {
+                if (b.charAt(0) == endChar) break;
+                b.remove(0, 1);
+            }
+
+            msg = a + value + b;
+            return;
+        }
+    }
+
+    msg.remove(msg.length() - 1);
+    msg = msg + (String)parameterIdentifier + value + (String)endChar;
+}
+
+void Command::delParam(const char parameterIdentifier) {
+    if (msg == "" || msg.length() < 7) return;
+
+    for (int i = 4; i <= msg.length(); i++) {
+        if ((char)msg.charAt(i) == startChar) {
+            while ((char)msg.charAt(i) != endChar) {
+                i++;
+                // TODO: cmdAsParameter = true;
+            }
+        }
+
+        if (isUppercaseLetter(msg.charAt(i)) && msg.charAt(i) == parameterIdentifier) {
+            while (!isUppercaseLetter(msg.charAt(i + 1))) {
+                if (msg.charAt(i) == endChar) return;
+                msg.remove(i, 1);
+            }
+            msg.remove(i, 1);
+            return;
+        }
+    }
+}
+
+bool Command::hasParam(const char parameterIdentifier) {
+    for (int i = 4; i <= msg.length(); i++) {
+        if ((char)msg.charAt(i) == startChar) {
+            while ((char)msg.charAt(i) != endChar) {
+                i++;
+                // TODO: cmdAsParameter = true;
+            }
+        }
+
+        if (isUppercaseLetter(msg.charAt(i)) && msg.charAt(i) == parameterIdentifier) return true;
+    }
+    return false;
+}
+
+
 
 
 bool Command::isUppercaseLetter(char m) {
     return m >= 65 && m <= 90;
 }
 
-
 String Command::toString() {
     return (String)msg;
 }
-
-
